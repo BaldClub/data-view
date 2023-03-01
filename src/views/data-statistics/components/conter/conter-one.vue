87f8ce0443024da432b1450ec3f9cdaa1f39bd9f
kaito kuroba <57478299+bzpen@users.noreply.github.com>
2023-03-01 19:31:13 +0800

    feat: 数据统计页面开发 (#9)
    * feat: 数据统计页面开发
    
    * Update src/views/data-statistics/components/right/right-one.vue
    
    Co-authored-by: Leo <64216877+wangrunlin@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leo <64216877+wangrunlin@users.noreply.github.com>

---
+++ src/views/data-statistics/components/conter/conter-one.vue
@@ -0,0 +1,111 @@
+<!--
+ * @author: kaito-kuroba
+ * @since: 2023-03-01
+ * conter-one.vue
+-->
+<template>
+  <div class="container-item" :style="{height: maxHeight + 'px'}">
+        <div class="redius-bg" :style="{height: maxHeight * 0.6 + 'px'}">
+            <div class="item" v-for="item,index in dataList" :style="{'background-image': `linear-gradient(to right, ${item.color[0]}, ${item.color[0]})`, 
+                    top: ((index) / 3) * 50 % (100 - item.num / 1.4) + '%',
+                    left: ((index) % 3) * 50 % (100 - item.num / 1.5) + '%',
+                    height: item.num  + '%',
+                    width: item.num + '%'
+                }">
+                {{ item.name }}
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['maxHeight'],
+    data(){
+        return {
+            dataList: [
+                {
+                    name: '光照',
+                    color:[
+                        'rgb(128, 255, 165)',
+                        'rgb(1, 191, 236)'
+                    ],
+                    num: 60
+                },
+                {
+                    name: '光照',
+                    color:[
+                        'rgb(0, 221, 255)',
+                        'rgb(77, 119, 255)'
+                    ],
+                    num: 10
+                },
+                {
+                    name: '光照',
+                    color:[
+                        'rgb(255, 162, 0)',
+                        'rgb(255, 231, 179)'
+                    ],
+                    num: 5
+                },
+                {
+                    name: '光照',
+                    color:[
+                        'rgb(255, 0, 135)',
+                        'rgb(135, 0, 157)'
+                    ],
+                    num: 15
+                },
+                {
+                    name: '光照',
+                    color:[
+                        'rgb(255, 191, 0)',
+                        'rgb(224, 62, 76)'
+                    ],
+                    num: 10
+                },
+                {
+                    name: '光照',
+                    color:[
+                        'rgb(255, 0, 191)',
+                        'rgb(255, 179, 200)'
+                    ],
+                    num: 60
+                }
+            ],
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+.container-item{
+    width: 100%;
+    position: relative;
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .redius-bg{
+        background-color: #e1e2e3bd;
+        width: 90%;
+        border-radius: 50%;
+        position: relative;
+        .item{
+            width: 40px;
+            height: 100px;
+            opacity: 0.8;
+            position: absolute;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #fff;
+            font-size: 2rem;
+            &:hover{
+                opacity: 1;
+            }
+        }
+    }
+}
+</style>
---
+++ src/views/data-statistics/components/left/left-two.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="content">
+        <h2>工厂设备数量</h2>
+        <div ref="echarts" :style="{height: maxHeight * 0.9 + 'px'}">
+  
+        </div>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      props: [ 'maxHeight' ],
+      data(){
+        return {
+          echarts:'',
+          option :{
+            xAxis: {
+              type: 'category',
+              name: '单位（月）',
+              axisLabel: {
+                  show: true,
+                  textStyle: {
+                      color: '#ffffff'
+                  }
+              },
+              data: ['一月', '二月', '三月', '四月', '五月', '六月', '七月','八月','九月','十月','十一月','十二月']
+            },
+            yAxis: {
+              type: 'value',
+              name: '单位（次）',
+              axisLabel: {
+                  show: true,
+                  textStyle: {
+                      color: '#ffffff'
+                  }
+              },
+            },
+            legend: {
+              data: ['总产量', '产值'],
+              textStyle: {
+                color: '#fff'
+                // ...
+              }
+            },
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                type: 'shadow'
+              }
+            },
+            grid:{
+                top: 40,
+                right: 70,
+                bottom: 50
+            },
+            series: [
+              {
+                name: '产值',
+                color: ['rgb(128, 255, 165)'],
+                data: [150, 230, 224, 218, 135, 147, 260,150, 230, 224, 218, 135, 147, ],
+                type: 'line'
+              }
+            ]
+          }
+        }
+      },
+      mounted(){
+          this.initCharts()
+      },
+      methods:{
+        initCharts(){
+            this.echarts = this.$echarts.init(this.$refs.echarts) 
+            this.echarts.setOption(this.option)
+        }
+      }
+    }
+  </script>
+  
+  <style lang="less" scoped>
+  .content{
+    h2{
+      padding-left: 0.6em;
+      text-align: left;
+    }
+  }
+  </style>
+  
