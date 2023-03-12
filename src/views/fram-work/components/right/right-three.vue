bbe936064e3c50da7a4c40e4b7a383b4265b70ac
kaito_kuroba <3118659412@qq.com>
2023-03-12 14:11:09 +0800

    feat: 农资农事界面开发

--- src/views/fram-work/components/right/right-three.vue
+++ src/views/fram-work/components/right/right-three.vue
@@ -1,97 +1,91 @@
 <template>
-    <div class="content">
-        <h2>预警报警次数</h2>
-        <div ref="echarts" :style="{height: `${maxHeight * 0.9}px`}">
-  
-        </div>
+  <div class="content">
+    <h2>预警报警次数</h2>
+    <div ref="echarts" :style="{ height: `${maxHeight * 0.9}px` }">
+
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-    export default {
-      props: [ 'maxHeight' ],
-      data(){
-        return {
-          echarts:'',
-          option :{
-            xAxis: {
-              type: 'category',
-              name: '单位（月）',
-              axisLabel: {
-                  show: true,
-                  textStyle: {
-                      color: '#ffffff'
-                  }
-              },
-              data: ['一月', '二月', '三月', '四月', '五月', '六月', '七月','八月','九月','十月','十一月','十二月']
-            },
-            yAxis: {
-              type: 'value',
-              name: '单位（次）',
-              axisLabel: {
-                  show: true,
-                  textStyle: {
-                      color: '#ffffff'
-                  }
-              },
-            },
-            tooltip: {
-              trigger: 'axis',
-              axisPointer: {
-                type: 'shadow'
-              }
-            },
-            grid:{
-                top: 40,
-                right: 70,
-                bottom: 50
-            },
-            series: [
-              {
-                itemStyle: {
-                  color: {
-                    type: 'linear',
-                    x: 0,
-                    y: 0,
-                    x2: 0,
-                    y2: 1,
-                    colorStops: [
-                      {
-                          offset: 0, color: 'rgb(255, 0, 135)' // 0% 处的颜色
-                      }, 
-                      {
-                          offset: 1, color: 'rgb(135, 0, 157)' // 100% 处的颜色
-                      },
-                     
-                    ],
-                    global: false // 缺省为 false
-                  }
-                },
-                data: [120, 200, 150, 80, 70, 110, 130, 13, 130, 130, 130, 130],
-                type: 'bar'
-              }
-            ]
+<script>
+export default {
+  props: ['maxHeight'],
+  data() {
+    return {
+      echarts: '',
+      option: {
+        xAxis: {
+          type: 'category',
+          name: '单位（月）',
+          axisLabel: {
+            show: true,
+            textStyle: {
+              color: '#ffffff'
+            }
+          },
+          data: []
+        },
+        yAxis: {
+          type: 'value',
+          name: '单位（次）',
+          axisLabel: {
+            show: true,
+            textStyle: {
+              color: '#ffffff'
+            }
+          },
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
           }
-        }
-      },
-      mounted(){
-          this.initCharts()
-      },
-      methods:{
-        initCharts(){
-            this.echarts = this.$echarts.init(this.$refs.echarts) 
-            this.echarts.setOption(this.option)
-        }
+        },
+        grid: {
+          top: 40,
+          right: 70,
+          bottom: 50
+        },
+        series: [
+          {
+            itemStyle: {
+              color: '#ba7f86'
+            },
+            data: [2,1,1],
+            type: 'bar'
+          }
+        ]
       }
     }
-  </script>
-  
-  <style lang="less" scoped>
-  .content{
-    h2{
-      padding-left: 0.6em;
-      text-align: left;
+  },
+  mounted() {
+    this.initData()
+    this.initCharts()
+  },
+  methods: {
+    initCharts() {
+      this.echarts = this.$echarts.init(this.$refs.echarts)
+      this.echarts.setOption(this.option)
+    },
+    // 数据初始化
+    initData() {
+      let xData = []
+      let now = new Date().getMonth();
+      for (var i = 0; i <= now; i++) {
+        xData.push((i + 1) + '月')
+      }
+      this.option.xAxis.data = xData
     }
   }
-  </style>
+}
+</script>
+  
+<style lang="less" scoped>
+.content {
+  h2 {
+    padding-left: 0.6em;
+    text-align: left;
+  }
+}
+</style>
   
