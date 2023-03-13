3838c658d7a3dbf0a4c8698060cbbd25e1bfdbcf
kaito_kuroba <3118659412@qq.com>
2023-03-13 20:21:21 +0800

    fix: 界面修改完成（截屏完成）

--- src/views/production/components/conter/conter-one.vue
+++ src/views/production/components/conter/conter-one.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="content">
-    <div class="video" v-for="item in 4">
+    <div class="video">
+      <img style="width: 100%" src="@/assets/images/微信图片_20230313122833.png" alt="">
+    </div>
+    <div class="video">
+      <img style="width: 100%" src="@/assets/images/微信图片_20230313122859.png" alt="">
+    </div>
+    <div class="video">
+      <img style="width: 100%" src="@/assets/images/微信图片_20230313122853.png" alt="">
+    </div>
+    <div class="video">
       <video width="100%" controls>
         <source
-          src="http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
+          src="#"
           type="video/mp4"
         />
       </video>
