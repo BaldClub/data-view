c0ee3ec838963193da137ff753d5b5af5c1f3115
kaito_kuroba <3118659412@qq.com>
2023-05-25 22:42:31 +0800

    feat: 完善视频加载

--- src/components/videoBox.vue
+++ src/components/videoBox.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="content">
+        <div class="loading-box" v-show="show">
+            <loading />
+        </div>
         <div class="video">
-            <video width="100%" loop autoplay muted="false">
+            <video width="100%" @loadedmetadata="onloadeddata" ref="video" loop autoplay muted="false">
                 <source :src="url" type="video/mp4" />
             </video>
         </div>
@@ -10,55 +13,66 @@
         </div>
     </div>
 </template>
-  
+
 <script>
-  export default {
-    props: ["url"],
+import loading from './loading.vue';
+export default {
+    props: ['url'],
+    components: { loading },
     data() {
-      return {
-        time: '2023 / 05 / 23  22:30:33',
-        interval : null
+        return {
+            time: '',
+            interval: null,
+            show: true,
+        };
+    },
+    destroyed() {
+        clearInterval(this.interval);
+    },
+    methods: {
+        initTime() {
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth();
+            const day = date.getDate();
+            const time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
 
-      };
+            this.time = `${year} / ${month} / ${day}  ${time}`;
+        },
+        // 加载完成执行
+        onloadeddata() {
+            this.show = false;
+            this.interval = setInterval(() => {
+                this.initTime();
+            }, 1000);
+        },
     },
-    mounted(){
-        if(this.url !== '/'){
-            this.interval = setInterval(()=>{
-                this.initTime()
-            },1000)
-        }else{
-            this.time = ''
-        }
-    },
-    destroyed(){
-        clearInterval(this.interval)
-    },
-    methods:{
-        initTime(){
-            let date = new Date();
-            let year = date.getFullYear() 
-            let month = date.getMonth()
-            let day = date.getDate()
-            let time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+};
+</script>
 
-            this.time =  `${year} / ${month} / ${day}  ${time}`
-        }
-    }
-  };
-  </script>
-  
-  <style lang="less" scoped>
-  .content {
+<style lang="less" scoped>
+.loading-box {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background: #000000;
+}
+.content {
     width: 100%;
     height: 100%;
     position: relative;
     background-color: #000;
-  
+
     .video {
-      width: 100%;
-      margin: 0px auto;
+        width: 100%;
+        margin: 0px auto;
     }
-  
+
     .time {
         position: absolute;
         top: 10px;
@@ -67,6 +81,5 @@
         color: aliceblue;
         cursor: alias;
     }
-  }
-  </style>
-  
+}
+</style>
